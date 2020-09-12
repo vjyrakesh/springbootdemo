58224e24f1cb7221aef9e2653cf456da0a381c2f
Rakesh Kasibhatla <rkasibha@cisco.com>
2020-09-12 18:34:40 +0530

    Add IP to interceptor

--- src/main/java/com/rkasibha/rentabook/interceptor/AddTrackingIdInterceptor.java
+++ src/main/java/com/rkasibha/rentabook/interceptor/AddTrackingIdInterceptor.java
@@ -1,5 +1,8 @@
 package com.rkasibha.rentabook.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +12,8 @@ import java.util.UUID;
 
 public class AddTrackingIdInterceptor extends HandlerInterceptorAdapter {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AddTrackingIdInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String trackingIdHeader = request.getHeader("WEBEX_TRACKINGID");
@@ -17,7 +22,12 @@ public class AddTrackingIdInterceptor extends HandlerInterceptorAdapter {
             trackingIdHeader = "BOOKSTORE_" + UUID.randomUUID().toString();
             response.setHeader("WEBEX_TRACKINGID", trackingIdHeader);
         }
-
+        MDC.put("HOSTIP", request.getLocalAddr());
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        LOG.info("Completed request: " + request.getRequestURI());
     }
 }
